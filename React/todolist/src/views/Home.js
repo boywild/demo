2a3664704482d28b7bfee26a063b05cfe739cc98
chentian <chentian@kayak.com.cn>
2018-05-15 14:48:09 +0800

    Reset

--- React/todolist/src/views/Home.js
+++ React/todolist/src/views/Home.js
@@ -1,9 +1,11 @@
 import React,{Component} from 'react';
 
+
 class Home extends Component{
     render(){
         return(
-            <h1>Home</h1>
+            <div>asdfasdf
+            </div>
         );
     }
 }
