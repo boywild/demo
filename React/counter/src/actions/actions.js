1669fe4c0c314226038f902e954f28350327ad0a
boywild <1915951893@qq.com>
2018-06-20 23:27:54 +0800

    整理归档

--- React/counter/src/actions/actions.js
+++
@@ -1,7 +0,0 @@
-const INCREMENT='INCREMENT';
-const DECREMENT='DECREMENT';
-
-const increment=(count)=>({type:INCREMENT,count});
-const decrement=(count)=>({type:DECREMENT,count});
-
-export {INCREMENT,DECREMENT,increment,decrement};
