d23bd286f09f893c315475f185201812df154635
boywild <1915951893@qq.com>
2018-11-04 14:41:14 +0800

    小程序api测试

--- read-mini-program/pages/welcome/welcome.js
+++ read-mini-program/pages/welcome/welcome.js
@@ -1,4 +1,24 @@
 Page({
+    data: {
+        userInfo: {}
+    },
+    onLoad() {
+        const that = this;
+        wx.getSetting({
+            success(res) {
+                if (res.authSetting['scope.userInfo']) {
+                    wx.getUserInfo({
+                        success(res) {
+                            that.setData({
+                                userInfo: res.userInfo
+                            });
+                            console.log(res.userInfo);
+                        }
+                    })
+                }
+            }
+        })
+    },
     onTap: function(event) {
         // wx.navigateTo({
         //     url:"../posts/post"
