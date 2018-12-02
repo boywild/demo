7bd87e808b85f92a4b99a1172b804eb73fb3f87f
chentian <chentian.ct@qq.com>
2018-12-02 23:11:21 +0800

    chat

--- Node/lib/chat_server.js
+++ Node/lib/chat_server.js
@@ -1,9 +1,9 @@
-const socketio = require("socket.io");
+const socketio = require('socket.io');
 let io,
-  guestNumber = 1,
-  nickNames = {},
-  namesUsed = [],
-  currentRoom = {};
+    guestNumber = 1,
+    nickNames = {},
+    namesUsed = [],
+    currentRoom = {};
 
 /**
  * 分配昵称
@@ -14,16 +14,123 @@ let io,
  * 用户断开连接
  */
 exports.listen = function(server) {
-  io = socketio.listen(server);
-  io.set("log level", 1);
-  io.sockets.on("connection", function(socket) {
-    guestNumber = assignGuestName(socket, guestNumber, nickNames, namesUsed);
-    joinRoom(socket, "Lobby");
-    handleMessageBroadcasting(socket, nickNames);
-    handleRoomJoining(socket);
-    socket.on("rooms", function() {
-      socket.emit("rooms", io.socket.manager.rooms);
+    io = socketio.listen(server);
+    io.set('log level', 1);
+    io.on('connection', function(socket) {
+        guestNumber = assignGuestName(
+            socket,
+            guestNumber,
+            nickNames,
+            namesUsed
+        );
+        joinRoom(socket, 'Lobby');
+        handleMessageBroadcasting(socket, nickNames);
+        handleRoomJoining(socket);
+        socket.on('rooms', function() {
+            socket.emit('rooms', io.socket.manager.rooms);
+        });
+        handleClientDisconnection(socket, nickNames, namesUsed);
     });
-    handleClientDisconnection(socket, nickNames, namesUsed);
-  });
 };
+
+/**
+ * 分配昵称
+ */
+function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
+    const name = 'Guest' + guestNumber;
+    nickNames[socket.id] = name;
+    socket.emit('nameResult', {
+        success: true,
+        name: name
+    });
+    namesUsed.push(name);
+    return guestNumber + 1;
+}
+
+/**
+ * 加入房间
+ */
+function joinRoom(socket, room) {
+    socket.join(room);
+    currentRoom[socket.id] = room;
+    socket.emit('joinResult', { room: room });
+    socket.broadcast.to(room).emit('message', {
+        text: nickNames[socket.id] + 'has join in romm'
+    });
+    console.log(io);
+    const usersInRoom = io.Namespace.sockets.clients(room);
+    if (usersInRoom.length > 1) {
+        let usersInRoomSummary = 'users currently in ' + room + ':';
+        for (let index in usersInRoom) {
+            let userSocketId = userInRoom[index].id;
+            if (userSocketId !== socket.id) {
+                if (index > 0) {
+                    usersInRoomSummary += ',';
+                }
+                usersInRoomSummary += nickNames[userSocketId];
+            }
+        }
+        usersInRoomSummary += '.';
+        socket.emit('message', { text: usersInRoomSummary });
+    }
+}
+
+/**
+ * 昵称变更
+ */
+
+function handleNameChangeAttempts(socket, nickNames, namesUsed) {
+    socket.on('nameAttempt', function(name) {
+        if (name.indexOf('Guest') === 0) {
+            socket.emit('nameResult', {
+                success: false,
+                message: 'Names cannot begin with Guest'
+            });
+        } else {
+            if (name.indexOf(namesUsed) === -1) {
+                const previousName = nickNames[socket.id];
+                const previousNameIndex = namesUsed.indexOf();
+                namesUsed.push(name);
+                nickNames[socket.id] = name;
+                delete namesUsed[previousNameIndex];
+                socket.on('nameResult', {
+                    success: true,
+                    name: name
+                });
+            }
+        }
+    });
+}
+
+/**
+ * 发送聊天消息
+ */
+
+function handleMessageBroadcasting(socket) {
+    socket.on('message', function(message) {
+        socket.broadcast.to(message.room).emit('message', {
+            text: nickNames[socket.id] + ':' + message.text
+        });
+    });
+}
+
+/**
+ * 创建房间
+ */
+function handleRoomJoining(socket) {
+    socket.on('join', function(room) {
+        socket.leave(currentRoom[socket.id]);
+        joinRoom(socket, room.newRoom);
+    });
+}
+
+/**
+ * 用户断开连接
+ */
+function handleClientDisconnection(socket) {
+    socket.on('disconnect', function() {
+        let nameIndex = namesUsed.indexOf(nickNames[socket.id]);
+        delete namesUsed[nameIndex];
+        delete nickNames[socket.id];
+    });
+}
--- Node/server.js
+++ Node/server.js
@@ -1,57 +1,58 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-const mime = require("mime");
-const chatServer = require("./lib/chat_server");
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const chatServer = require('./lib/chat_server');
 
 let cache = {};
 
 function send404(response) {
-  response.writeHead("404", { "Content-type": "text/plain" });
-  response.write("Error 404:resource not found");
-  response.end();
+    response.writeHead('404', { 'Content-type': 'text/plain' });
+    response.write('Error 404:resource not found');
+    response.end();
 }
 
 function sendFile(response, filePath, fileContents) {
-  response.writeHead(200, {
-    "Content-type": mime.getType(path.basename(filePath))
-  });
-  response.end(fileContents);
+    response.writeHead(200, {
+        'Content-type': mime.getType(path.basename(filePath))
+    });
+    response.end(fileContents);
 }
 
 function serverStatic(response, cache, absPath) {
-  if (cache[absPath]) {
-    sendFile(response, absPath, cache[absPath]);
-  } else {
-    fs.exists(absPath, function(exists) {
-      if (exists) {
-        fs.readFile(absPath, function(err, data) {
-          if (err) {
-            send404(response);
-          } else {
-            cache[absPath] = data;
-            sendFile(response, absPath, data);
-          }
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
         });
-      } else {
-        send404(response);
-      }
-    });
-  }
+    }
 }
 
 const server = http.createServer(function(req, res) {
-  let filePath = false;
-  if (req.url === "/") {
-    filePath = "public/index.html";
-  } else {
-    filePath = "public" + req.url;
-  }
-  let absPath = "./" + filePath;
-  serverStatic(res, cache, absPath);
-});
-server.listen(3000, function() {
-  console.log("服务已启动，端口3000");
+    let filePath = false;
+    if (req.url === '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + req.url;
+    }
+    let absPath = './' + filePath;
+    serverStatic(res, cache, absPath);
 });
 chatServer.listen(server);
+server.listen(3000, function() {
+    console.log('服务已启动，端口3000');
+});
+
 console.log(path.basename);
