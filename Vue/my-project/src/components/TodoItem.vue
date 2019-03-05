c9106cc661c20a9e5086b75c8baf7c9e6376e808
chentian <chentian.ct@qq.com>
2019-03-05 19:59:46 +0800

    增加vue项目文件夹

---
+++ Vue/my-project/src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+	<li @click="deleteTodo">{{ content }}</li>
+</template>
+<script>
+export default {
+	name: "TodoItem",
+	props: ["content", "index"],
+	methods: {
+		deleteTodo() {
+			this.$emit("handleDelete", this.index);
+		}
+	}
+};
+</script>
+<style scoped>
+</style>
+
+
---
+++ Vue/my-project/src/components/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id="todo">
+		<input type="text" v-model="inputTxt" /><button @click="handleSubmit">
+			提交
+		</button>
+		<ul>
+			<todo-item
+				:key="index"
+				v-for="(item, index) of todo"
+				:content="item"
+				:index="index"
+				@handleDelete="deleteCurrentTodo"
+			></todo-item>
+		</ul>
+	</div>
+</template>
+<script>
+import TodoItem from "./TodoItem";
+export default {
+	name: "TodoList",
+	components: { "todo-item": TodoItem },
+	data() {
+		return {
+			todo: [],
+			inputTxt: ""
+		};
+	},
+	methods: {
+		handleSubmit() {
+			this.todo.push(this.inputTxt);
+			this.inputTxt = "";
+		},
+		deleteCurrentTodo(index) {
+			this.todo.splice(index, 1);
+		}
+	}
+};
+</script>
+
+<style scoped>
+</style>
+
+
