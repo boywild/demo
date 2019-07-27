145e631487064a65c4ef2b4f55600d0c69e6d482
chentian <chentian.ct@qq.com>
2019-07-27 15:13:46 +0800

    新增小程序天气项目

---
+++ quietweather/app.js
@@ -0,0 +1,26 @@
+App({
+  onLaunch () {
+    wx.cloud.init({
+      env: 'wx-cloud-17f015',
+      traceUser: true,
+    })
+    wx.getSystemInfo({
+      success: (res) => {
+        this.globalData.systeminfo = res
+        this.globalData.isIPhoneX = /iphonex/gi.test(res.model.replace(/\s+/, ''))
+      },
+    })
+  },
+  globalData: {
+    // 是否保持常亮，离开小程序失效
+    keepscreenon:false,
+    systeminfo: {},
+    isIPhoneX: false,
+    key: 'b1ac70360eed474089e717838a5e1866',
+    weatherIconUrl: 'https://cdn.heweather.com/cond_icon/',
+    requestUrl: {
+      weather: 'https://free-api.heweather.com/s6/weather',
+      hourly: 'https://free-api.heweather.com/s6/weather/hourly',
+    },
+  },
+})
